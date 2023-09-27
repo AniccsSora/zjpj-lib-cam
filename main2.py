f276856d010c7ffd0ecfda9dedb1d98eba51c57f
Anicca <aniccasora@gmail.coim>
2023-09-28 01:30:01 +0800

    優化校正顯示結果 與 初始顯示大小

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from pathlib import Path  # 匯入 Path 類
-
+from utils import *
 # 初始化全局變數
 points = []
 trans_matrix_G = None
@@ -24,7 +24,7 @@ def click_event(event, x, y, flags, param):
             trans_matrix_G = get_transform_matrix(img, points)
 
 # 執行透視變換
-def transform_image(img, trans_matrix):
+def transform_image(img, trans_matrix, exceed=1.2):
     # 複製原圖像以避免修改原始圖像
     transformed_img = img.copy()
 
@@ -32,15 +32,15 @@ def transform_image(img, trans_matrix):
     width, height = img.shape[1], img.shape[0]
     new_points = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
 
-    result = cv2.warpPerspective(img, trans_matrix, (width, height))
+    result = cv2.warpPerspective(img, trans_matrix, (int(width*exceed), int(height*exceed)), borderValue=(255, 255, 255))
 
     # 顯示變換後的圖像
-    cv2.imshow('Transformed Image', result)
+    cv2.imshow('Transformed Image', resize_image_with_max_resolution(result, 800))
 
     # 使用 Path 來設定保存圖像的路徑
     output_path = Path('./output')
     output_path.mkdir(parents=True, exist_ok=True)
-    cv2.imwrite(str(output_path.joinpath('transformed_image.jpg')) , result)
+    cv2.imwrite(str(output_path.joinpath('transformed_image.jpg')), result)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -60,8 +60,10 @@ def get_transform_matrix(img, points):
 
 if __name__ == "__main__":
     # 載入圖片
-    img_path = Path('./images/test1.png')  # 輸入圖片的路徑
-    img = cv2.imread(str(img_path))
+    #img_path = Path('./images/test1.png')  # 輸入圖片的路徑
+    img_path = Path('./images/zj_lab/1.jpg')  # 輸入圖片的路徑
+    img = resize_image_with_max_resolution(cv2.imread(str(img_path)), 800)
+    w, h = img.shape[1], img.shape[0]
     clean_img = img.copy()
 
     # 顯示圖片並設置點擊事件的回調函數
@@ -73,6 +75,14 @@ if __name__ == "__main__":
     cv2.destroyAllWindows()
 
     if trans_matrix_G is not None:
+        print(trans_matrix_G)
+        # 定義要向左上移動的偏移量
+        dx = int(w*0.2)  # 向左移動 50 像素
+        dy = int(h*0.2)  # 向上移動 50 像素
+
+        # 修改矩陣中的平移部分以實現左上移動
+        trans_matrix_G[0, 2] += dx
+        trans_matrix_G[1, 2] += dy
         transform_image(clean_img, trans_matrix_G)
     else:
         print("trans_matrix_G is None, exit program.")
---
+++ utils.py
@@ -0,0 +1,27 @@
+import cv2
+
+def resize_image_with_max_resolution(image, max_resolution):
+    """
+    調整圖片的大小，以限制最大一邊的解析度，並保持長寬比例。
+
+    :param image: 輸入圖片（NumPy 數組）
+    :param max_resolution: 最大解析度（一個整數，表示最大一邊的像素數）
+    :return: 調整大小後的圖片
+    """
+    # 獲取原始圖片的寬度和高度
+    original_height, original_width = image.shape[:2]
+
+    # 確定調整大小的目標寬度和高度
+    if original_width > original_height:
+        # 如果寬度大於高度，將寬度調整為最大解析度，並保持長寬比例
+        new_width = max_resolution
+        new_height = int(original_height * (max_resolution / original_width))
+    else:
+        # 如果高度大於寬度，將高度調整為最大解析度，並保持長寬比例
+        new_height = max_resolution
+        new_width = int(original_width * (max_resolution / original_height))
+
+    # 使用 OpenCV 的 resize 函數進行調整大小
+    resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    return resized_image
