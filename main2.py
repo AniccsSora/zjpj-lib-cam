abaa969a16e75c0804347fef641406b1fc13927d
Anicca <aniccasora@gmail.coim>
2023-09-28 00:24:26 +0800

    good to remove user assigned points.!!

--- main2.py
+++ main2.py
@@ -1,12 +1,14 @@
 import cv2
 import numpy as np
+from pathlib import Path  # 匯入 Path 類
 
-# 創建一個空的列表來存儲點的座標
+# 初始化全局變數
 points = []
+trans_matrix_G = None
 
 # 點擊事件的回調函數
 def click_event(event, x, y, flags, param):
-    global points
+    global points, trans_matrix_G
 
     if event == cv2.EVENT_LBUTTONDOWN:
         # 在圖片上畫一個小圓圈來標記點的位置
@@ -16,30 +18,51 @@ def click_event(event, x, y, flags, param):
         # 將點的座標添加到列表中
         points.append((x, y))
 
-        # 當收集到四個點時，進行透視變換並顯示結果
+        # 當收集到四個點時，進行透視變換並保存結果
         if len(points) == 4:
             cv2.destroyAllWindows()
-            transform_image(img, points)
+            trans_matrix_G = get_transform_matrix(img, points)
 
 # 執行透視變換
-def transform_image(img, points):
+def transform_image(img, trans_matrix):
+    # 複製原圖像以避免修改原始圖像
+    transformed_img = img.copy()
+
+    # 設置目標圖像的寬度和高度與原圖相同
+    width, height = img.shape[1], img.shape[0]
+    new_points = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+
+    result = cv2.warpPerspective(img, trans_matrix, (width, height))
+
+    # 顯示變換後的圖像
+    cv2.imshow('Transformed Image', result)
+
+    # 使用 Path 來設定保存圖像的路徑
+    output_path = Path('./output')
+    output_path.mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(str(output_path.joinpath('transformed_image.jpg')) , result)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def get_transform_matrix(img, points):
+    # 複製原圖像以避免修改原始圖像
+    transformed_img = img.copy()
+
     # 設置目標圖像的寬度和高度與原圖相同
     width, height = img.shape[1], img.shape[0]
     new_points = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
 
     # 執行透視變換
     matrix = cv2.getPerspectiveTransform(np.array(points, dtype=np.float32), new_points)
-    result = cv2.warpPerspective(img, matrix, (width, height))
 
-    # 顯示變換後的圖像
-    cv2.imshow('Transformed Image', result)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+    return matrix
 
 if __name__ == "__main__":
     # 載入圖片
-    img = cv2.imread('./images/test1.png')
+    img_path = Path('./images/test1.png')  # 輸入圖片的路徑
+    img = cv2.imread(str(img_path))
+    clean_img = img.copy()
 
     # 顯示圖片並設置點擊事件的回調函數
     cv2.imshow('image', img)
@@ -48,3 +71,8 @@ if __name__ == "__main__":
     # 等待用戶按下任意鍵來退出
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    if trans_matrix_G is not None:
+        transform_image(clean_img, trans_matrix_G)
+    else:
+        print("trans_matrix_G is None, exit program.")
