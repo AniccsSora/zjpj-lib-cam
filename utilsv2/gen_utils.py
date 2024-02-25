c391096755a1bf8c74d49ca33cb279d9f2dcb367
Anicca <aniccasora@gmail.coim>
2024-02-26 03:33:06 +0800

    update

--- utilsv2/gen_utils.py
+++ utilsv2/gen_utils.py
@@ -14,29 +14,54 @@ if 'Microsoft JhengHei' in _useable_fonts:
 
 def resize_max_res(image: np.ndarray, max_resolution: int = 800) -> np.ndarray:
     """
-    調整圖片的大小，以限制最大一邊的解析度，並保持長寬比例。
+     調整圖片的大小，以限制最大一邊的解析度，並保持長寬比例。如果最大邊小於 max_resolution，
+     則將最大邊調整至 max_resolution，同時保持長寬比。
 
-    :param image: 輸入圖片（NumPy 數組）
-    :param max_resolution: 最大解析度（一個整數，表示最大一邊的像素數）
-    :return: 調整大小後的圖片
-    """
-    # 獲取原始圖片的寬度和高度
+     :param image: 輸入圖片（NumPy 數組）
+     :param max_resolution: 最大解析度（一個整數，表示最大一邊的像素數）
+     :return: 調整大小後的圖片
+     """
     original_height, original_width = image.shape[:2]
+    max_side = max(original_height, original_width)
 
-    # 確定調整大小的目標寬度和高度
-    if original_width > original_height:
-        # 如果寬度大於高度，將寬度調整為最大解析度，並保持長寬比例
-        new_width = max_resolution
-        new_height = int(original_height * (max_resolution / original_width))
+    # 判斷是否需要放大
+    if max_side < max_resolution:
+        # 放大至 max_resolution，同時保持長寬比
+        scale_factor = max_resolution / max_side
     else:
-        # 如果高度大於寬度，將高度調整為最大解析度，並保持長寬比例
-        new_height = max_resolution
-        new_width = int(original_width * (max_resolution / original_height))
+        # 縮小至 max_resolution，同時保持長寬比
+        scale_factor = max_resolution / max_side
 
-    # 使用 OpenCV 的 resize 函數進行調整大小
+    new_width = int(original_width * scale_factor)
+    new_height = int(original_height * scale_factor)
+
     resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
     return resized_image
+    # """
+    # 調整圖片的大小，以限制最大一邊的解析度，並保持長寬比例。
+    #
+    # :param image: 輸入圖片（NumPy 數組）
+    # :param max_resolution: 最大解析度（一個整數，表示最大一邊的像素數）
+    # :return: 調整大小後的圖片
+    # """
+    # # 獲取原始圖片的寬度和高度
+    # original_height, original_width = image.shape[:2]
+    #
+    # # 確定調整大小的目標寬度和高度
+    # if original_width > original_height:
+    #     # 如果寬度大於高度，將寬度調整為最大解析度，並保持長寬比例
+    #     new_width = max_resolution
+    #     new_height = int(original_height * (max_resolution / original_width))
+    # else:
+    #     # 如果高度大於寬度，將高度調整為最大解析度，並保持長寬比例
+    #     new_height = max_resolution
+    #     new_width = int(original_width * (max_resolution / original_height))
+    #
+    # # 使用 OpenCV 的 resize 函數進行調整大小
+    # resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    #
+    # return resized_image
 
 
 def plot_images(images: List[np.ndarray],
--- utilsv2/table/table_highlighter.py
+++ utilsv2/table/table_highlighter.py
@@ -20,44 +20,126 @@ def detect_edges(gray_image: np.ndarray) -> np.ndarray:
 
 def detect_Hough_lines(edges: np.ndarray) -> np.ndarray:
     """使用 Hough 變換檢測直線"""
-    return cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=10)
+    return cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=50)
+
+def detect_Hough_lines_v2(edges: np.ndarray) -> np.ndarray:
+    """使用 Hough 變換檢測直線
+    但是會將圖片放大至 1000 up 再次做計算
+    """
+    edges = resize_max_res(edges, 1000)
+    return detect_Hough_lines(edges)
 
 
-def draw_lines(image, lines):
+def draw_lines(image, lines, color=(0, 255, 0), thickness=2):
     """在圖像上繪製直線"""
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(image, (x1, y1), (x2, y2), color, thickness)
     return image
 
 
-def resize_image(image, scale_percent=50):
-    """調整圖像大小"""
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+def erode_dilate_image(image: np.ndarray,
+                       erode_iterations: int = 1,
+                       dilate_iterations: int = 1,
+                       first_operation: str = 'erode') -> np.ndarray:
+    """
+    對圖片進行等量的侵蝕和膨脹操作。
+
+    :param image: 輸入的圖片（NumPy 數組）。
+    :param iterations: 侵蝕和膨脹操作的次數。
+    :param first_operation: 首先執行的操作，可以是 'erode'(侵蝕) 或 'dilate'(膨脹)。
+    :return: 處理後的圖片。
+    # example
+    #做個膨脹後侵蝕
+    erode_iter = 3   # '侵蝕' 操作的次數
+    dilate_iter = 3  # '膨脹' 操作的次數
+    edges_dilate_erode = erode_dilate_image(edges,
+                                            erode_iterations=erode_iter,
+                                            dilate_iterations=dilate_iter,
+                                            first_operation='dilate')
+
+    """
+    image = image.copy()
+    # 定義結構元素
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    # 根據參數選擇先進行哪種操作
+    if first_operation == 'erode':
+        image = cv2.erode(image, kernel, iterations=erode_iterations)
+        image = cv2.dilate(image, kernel, iterations=dilate_iterations)
+    elif first_operation == 'dilate':
+        image = cv2.dilate(image, kernel, iterations=dilate_iterations)
+        image = cv2.erode(image, kernel, iterations=erode_iterations)
+    else:
+        raise ValueError("first_operation must be 'erode' or 'dilate'")
+
+    return image
+
+
+def filter_hough_lines(hough_lines: np.ndarray, filter_q)-> np.ndarray:
+    """過濾 Hough 變換檢測到的直線"""
+    filtered_lines = []
+    for line in hough_lines:
+        x1, y1, x2, y2 = line[0]
+        # cals p1 & p2 distance
+        distance = int(np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+        filtered_lines.append(distance)
+    #
+    # 用 matplotlib 來繪製各長度區間直方圖
+
+    filtered_lines = sorted(filtered_lines)
+    remove_len = np.percentile(filtered_lines, filter_q)
+    #print("remove length: ", remove_len)
+
+    filtered_lines_2 = []
+    for line in hough_lines:
+        x1, y1, x2, y2 = line[0]
+        # cals p1 & p2 distance
+        distance = int(np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+        if distance > remove_len:
+            filtered_lines_2.append(line[0])
+
+    # list to reshape like input hough_lines's ndarray.
+    ret = np.array(filtered_lines_2, dtype=np.int32).reshape((-1, 1, 4))
+    return ret
 
 
 if __name__ == "__main__":
     image_path = './images/nobody_lib_table.jpg'
     image = read_image(image_path)
     gray_image = convert_to_gray(image)
+    # 邊緣檢測
     edges = detect_edges(gray_image)
+    # hough 找出線段
     hough_line = detect_Hough_lines(edges)
+    # 過濾後的 hough 線段組
+    hough_line_filter = filter_hough_lines(hough_line, filter_q=30)
+    # 繪製直線
+    hough_line_image = draw_lines(np.zeros_like(image, dtype=np.uint8), hough_line)
+    # 繪製直線 (過濾後)
+    hough_line_image_filter = draw_lines(np.zeros_like(image, dtype=np.uint8), hough_line_filter)
+    #
     image_with_lines = draw_lines(image.copy(), hough_line)
+    blank_image = np.zeros_like(image, dtype=np.uint8)
     resized_image = resize_max_res(image_with_lines, 800)
 
     # 顯示每一步的結果
     cv2.imshow('Original Image', resize_max_res(image))
     cv2.imshow('Gray Image', resize_max_res(gray_image))
+    cv2.imshow('Hough Line', resize_max_res(hough_line_image))
     cv2.imshow('Edges', resize_max_res(edges))
     cv2.imshow('Lines Detected', resized_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     plot_images(
-        [image, gray_image, edges, image_with_lines],
-        [f'原始圖像_{image.ndim}', f'灰度圖_{gray_image.ndim}', '邊緣檢測', '檢測到的直線']
+        images=[image, gray_image,
+                edges, hough_line_image_filter,
+                hough_line_image, image_with_lines,
+                ],
+        titles=[f'原始圖像_{image.ndim}', f'灰度圖_{gray_image.ndim}',
+                '邊緣檢測', 'hough_line_image_filter',
+                'Hough Lines', '檢測到的直線',
+                ]
     )
 
